ac6f4717cab6d32e59e25a74a7ad13c3bc7b54a3
taka0628 <takatsun15@gmail.com>
2023-11-27 12:38:58 +0900

    [add]macOS用ファイル探索

--- internal/scripts/file-explorer-for-mac.sh
+++ internal/scripts/file-explorer-for-mac.sh
@@ -4,24 +4,17 @@ set -eu
 # macOSはBashコマンドがLinuxのものと互換性がないので，一部のスクリプトが動作しない
 # なのでmacOSではスクリプト実行用のコンテナ内でスクリプトを動作させることで回避
 
-readonly CONTAINER_NAME=bash-container
-
+readonly CONTAINER_NAME=python-container
 readonly CONTAINER_USER_DIR=/home/nobody
+readonly DOCKER_IMAGE=python:3.9.18-slim
 
 
 # このファイルが格納されているディレクトリの絶対パス
-readonly DIR_PATH=$(readlink -f $(dirname ${0}) | rev | cut -d "/" -f 1- | rev )
+readonly DIR_PATH=$(readlink -f $(dirname ${0}) | rev | cut -d "/" -f 4- | rev)
 
 
-if [[ -z $(docker ps | grep ${CONTAINER_NAME}) ]]; then
+[[ -z $(docker ps | grep ${CONTAINER_NAME}) ]] \
+	&& docker run -it --rm -d --name ${CONTAINER_NAME} -v /home/taka/git/semi-latex:${CONTAINER_USER_DIR}/semi-latex:ro ${DOCKER_IMAGE}
 
-	docker run -it --rm -d \
-		--name ${CONTAINER_NAME}\
-		-v ${DIR_PATH}/../../../semi-latex:${CONTAINER_USER_DIR}:ro \
-		bash:devel-alpine3.18
-
-fi
-
-result=$(docker exec -it ${CONTAINER_NAME} bash -c "cd ${CONTAINER_USER_DIR} && bash internal/scripts/file-explorer.sh" | tail -n 1)
-
-echo $result | sed -e "s@${CONTAINER_USER_DIR}/@@g"
+temp=$(docker exec ${CONTAINER_NAME} bash -c "cd ${CONTAINER_USER_DIR} && python3 semi-latex/internal/scripts/target_tex_find.py semi-latex")
+echo ${DIR_PATH}/${temp}
