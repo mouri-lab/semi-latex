46097d7fa733ba5c0a61a967dfc6f59fb3c17446
taka0628 <takatsun15@gmail.com>
2024-03-23 21:46:34 +0900

    [fix] 参照されるpathによって実行できなくなる問題を修正

--- internal/local/config.sh
+++ internal/local/config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+cd $(dirname $(readlink -f $0))/../../
 readonly CONTAINER_NAME=latex-container
-readonly STYLE_DIR=internal/container/style
-readonly SCRIPTS_DIR=internal/local
+readonly STYLE_DIR=$(readlink -f internal/container/style)
+readonly SCRIPTS_DIR=$(readlink -f internal/local)
 readonly DOCKER_HOME_DIR=/home/guest
 readonly ARCH=$(uname -m)
+cd -
